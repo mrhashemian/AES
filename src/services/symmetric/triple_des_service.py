ac556517aa6f61e5ab61db6c357a1ade6b9c62a3
mrhashemian <mr.hashemian2000@gmail.com>
2022-06-30 12:34:42 +0430

    refactor symmetric services

--- src/services/symmetric/triple_des_service.py
+++ src/services/symmetric/triple_des_service.py
@@ -1,4 +1,3 @@
-from Crypto.Hash import SHA256
 from typing_extensions import Literal
 from services.base_service import BaseService
 from Crypto import Random
@@ -11,8 +10,7 @@ class TripleDESService(BaseService):
     def __init__(self, mode: Literal["encrypt", "decrypt"], text, key=None):
         self.mode = mode
         self.text = text
-        self.key = SHA256.new(data=key.encode()).digest() if key else None
-        print(self.key)
+        self.key = key
         super().__init__(block_size=DES3.block_size)
 
     def encrypt(self, plain_text):
@@ -27,14 +25,14 @@ class TripleDESService(BaseService):
         iv = Random.new().read(self.block_size)
         cipher = DES3.new(self.key, DES3.MODE_CBC, iv)
         encrypted_text = cipher.encrypt(plain_text.encode())
-        return b64encode(iv + encrypted_text).decode("utf-8")
+        return b64encode(iv + encrypted_text).decode("utf-8"), self.key
 
-    # def decrypt(self, encrypted_text):
-    #     encrypted_text = b64decode(encrypted_text)
-    #     iv = encrypted_text[:self.block_size]
-    #     cipher = AES.new(self.key, AES.MODE_CBC, iv)
-    #     plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
-    #     return self.__unpad(plain_text)
+    def decrypt(self, encrypted_text):
+        encrypted_text = b64decode(encrypted_text)
+        iv = encrypted_text[:self.block_size]
+        cipher = DES3.new(self.key, DES3.MODE_CBC, iv)
+        plain_text = cipher.decrypt(encrypted_text[self.block_size:]).decode("utf-8")
+        return self._unpad(plain_text)
 
     def validate(self):
         pass
@@ -42,10 +40,11 @@ class TripleDESService(BaseService):
     def process(self):
         if self.mode == "encrypt":
             return self.encrypt(self.text)
-        # else:
-        #     self.decrypt(self.text)
+        return self.decrypt(self.text)
 
 
 if __name__ == "__main__":
-    aes = TripleDESService(mode="encrypt", text="aaa")
-    print(aes.execute())
+    des_encrypted, key = TripleDESService(mode="encrypt", text="aaa").execute()
+    print(des_encrypted, key)
+    des_decrypted = TripleDESService(mode="decrypt", text=des_encrypted, key=key)
+    print(des_decrypted.execute())
